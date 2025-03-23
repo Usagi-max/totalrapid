8754196fc35e17cd373376dba014736fb8c0c195
Usagi-max <totalrapideducation@gmail.com>
2025-03-23 20:12:19 +0900

    Update next.config.js for static export

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   trailingSlash: true, // URLの末尾にスラッシュを付ける設定
-  //output: 'export',  // 静的サイトをエクスポート
+  output: 'export',  // 静的サイトをエクスポート
 
   images: {
     unoptimized: true, // 画像の最適化を無効化
