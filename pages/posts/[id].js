b75000bf4ff5c9a8b6fd7b5a9861650a552cd878
Usagi-max <totalrapideducation@gmail.com>
2025-03-23 17:33:36 +0900

    Fix fallback in getStaticPaths

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,41 +1,40 @@
-import Link from "next/link"
-import Layout from "../../components/Layout"
-import { getAllPostIds, getPostData } from "../../lib/post"
+import Link from "next/link";
+import Layout from "../../components/Layout";
+import { getAllPostIds, getPostData } from "../../lib/post";
 import UtilStyles from "../../src/styles/utils.module.css";
 import Head from "next/head";
 
 export async function getStaticPaths() {
-    const paths = getAllPostIds()
+    const paths = getAllPostIds();
 
     return {
         paths,
-        fallback: blocking,
+        fallback: 'blocking', // ここを文字列 'blocking' に修正
     };
 }
 
-export async function getStaticProps({ params }){
+export async function getStaticProps({ params }) {
     const postData = await getPostData(params.id);
 
     return {
-        props:{
+        props: {
             postData,
         },
     };
 }
 
-export default function Post({postData}) {
+export default function Post({ postData }) {
     return (
         <Layout>
             <Head>
                 <title>{postData.title}</title>
-                <title>{postData.thumbnail}</title>
+                <meta name="thumbnail" content={postData.thumbnail} />
             </Head>
             <article>
-                
-                <h1 className = {UtilStyles.headingX1}>{postData.title}</h1>
+                <h1 className={UtilStyles.headingX1}>{postData.title}</h1>
                 <img src={postData.thumbnail} alt="Thumbnail" />
-                <div className = {UtilStyles.lightText}>{postData.date}</div>
-                <div dangerouslySetInnerHTML={{__html: postData.blogContentHTML}}/>
+                <div className={UtilStyles.lightText}>{postData.date}</div>
+                <div dangerouslySetInnerHTML={{ __html: postData.blogContentHTML }} />
             </article>
         </Layout>
     );
